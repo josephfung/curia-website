25bda712faf20a9cc653b132289720f1489c1736
Joseph Fung <joseph@josephfung.ca>
2026-06-24 16:25:16 -0400

    feat: build /capabilities page with eight responsibilities

---
+++ src/components/CapabilityDetail.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+// Normalise description to an array of paragraph strings regardless of input type
+const toParas = (d) => (Array.isArray(d) ? d : [d]);
+
+const CapabilityDetail = ({ heading, description, sequence, makesItWork, bringsYouIn, result }) => (
+  <article className="m-capdetail">
+    <h3 className="m-capdetail-h">{heading}</h3>
+    {toParas(description).map((p, i) => <p className="m-pillar-body" key={i}>{p}</p>)}
+    {sequence && (
+      <ol className="m-capdetail-seq">
+        {sequence.map((s, i) => <li key={i}>{s}</li>)}
+      </ol>
+    )}
+    <div className="m-capdetail-cols">
+      <div>
+        <div className="m-capdetail-label">What makes it work</div>
+        <ul className="m-capdetail-list">{makesItWork.map((m, i) => <li key={i}>{m}</li>)}</ul>
+      </div>
+      <div>
+        <div className="m-capdetail-label">Where Curia brings you in</div>
+        <ul className="m-capdetail-list">{bringsYouIn.map((m, i) => <li key={i}>{m}</li>)}</ul>
+      </div>
+    </div>
+    <p className="m-capdetail-result">{result}</p>
+  </article>
+);
+
+export default CapabilityDetail;
